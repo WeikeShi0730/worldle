78879e4394eac34a366f85a264a37eb02ecfd248
Weike Shi <weikeshih@gmail.com>
2022-05-10 21:52:06 -0400

    Add current country component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import CurrentCountry from "./components/current-country.component";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <CurrentCountry />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/current-country.component.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CurrentCountry = () => {
+  return <div>CurrentCountry</div>;
+};
+
+export default CurrentCountry;
