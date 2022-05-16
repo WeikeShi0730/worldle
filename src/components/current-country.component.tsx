5688788c259cfc2017cee0c69dd2be951fe9599b
Weike Shi <weikeshih@gmail.com>
2022-05-15 22:43:37 -0400

    Add country svg

--- src/components/current-country.component.tsx
+++ src/components/current-country.component.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { default as CountrySvg } from "../data/countries-svg/ca/vector.svg";
 
-// country image api:  
+// country image api:
 const CurrentCountry = () => {
-  return <div>CurrentCountry</div>;
+  return (
+    <div>
+      CurrentCountry
+      <div className="w-10 h-10">
+        <img src={CountrySvg} alt="Country SVG"/>
+        {/* <CountrySvg /> */}
+      </div>
+    </div>
+  );
 };
 
 export default CurrentCountry;
