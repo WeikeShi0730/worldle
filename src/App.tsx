d7b41ca58dd66915d8cb20d84f3ffa27c8252158
Weike Shi <weikeshih@gmail.com>
2022-05-16 23:18:23 -0400

    Add app context state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { useState, createContext } from "react";
+
 import CurrentCountry from "./components/current-country.component";
 import GuessPanels from "./components/guess-panels.component";
 import Selections from "./components/selections.component";
 
 function App() {
+  const SelectionsContext = createContext({});
+  const [numGuesses, setNumGuesses] = useState<number>(0);
+  const value = { numGuesses, setNumGuesses };
   return (
     <div>
-      <CurrentCountry />
-      <GuessPanels />
-      <Selections />
+      <SelectionsContext.Provider value={value}>
+        <CurrentCountry />
+        <GuessPanels />
+        <Selections />
+      </SelectionsContext.Provider>
     </div>
   );
 }
--- src/components/guess-panel.component.tsx
+++ src/components/guess-panel.component.tsx
@@ -1,9 +1,9 @@
 const GuessPanel = () => {
   return (
-    <div className="flex justify-evenly items-center max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg m-auto p-1 border-2 rounded">
-      <div className="country">Canada</div>
-      <div className="distance">1234km</div>
-      <div className="direction">⬇️</div>
+    <div className="flex justify-evenly items-center h-10 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg m-auto p-1 border-2 rounded">
+      <div className="country "></div>
+      <div className="distance"></div>
+      <div className="direction"></div>
     </div>
   );
 };
