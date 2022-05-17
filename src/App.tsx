0578d4c16345a53a28dfb159cbd629281087c829
Weike Shi <weikeshih@gmail.com>
2022-05-16 23:02:06 -0400

    Add guess panels

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import CurrentCountry from "./components/current-country.component";
+import GuessPanels from "./components/guess-panels.component";
 
 function App() {
   return (
     <div>
       <CurrentCountry />
+      <GuessPanels />
     </div>
   );
 }
