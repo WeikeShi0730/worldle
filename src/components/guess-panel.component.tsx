98c2abbee8f51e8a630e8cf0d0353c48914b4414
Weike Shi <weikeshih@gmail.com>
2022-05-18 23:50:37 -0400

    Fix guess-panel css

--- src/components/guess-panel.component.tsx
+++ src/components/guess-panel.component.tsx
@@ -1,9 +1,9 @@
 const GuessPanel = ({ selectedCountry }: any) => {
   return (
     <div className="flex justify-evenly items-center h-10 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg m-auto p-1 border-2 rounded">
-      <div className="country ">{selectedCountry.label}</div>
-      <div className="distance"></div>
-      <div className="direction"></div>
+      <div className="country flex justify-center w-8/12">{selectedCountry.label}</div>
+      <div className="distance flex justify-center w-2/12">1250km</div>
+      <div className="direction flex justify-center w-2/12">⬇️</div>
     </div>
   );
 };
