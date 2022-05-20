94157dffaf707e7928c5c3363d678ec570dffd18
Weike Shi <weikeshih@gmail.com>
2022-05-19 23:28:45 -0400

    Fix image size

--- src/components/current-country.component.tsx
+++ src/components/current-country.component.tsx
@@ -3,7 +3,7 @@ import { default as CountrySvg } from "../data/countries-svg/ca/vector.svg";
 const CurrentCountry = () => {
   return (
     <div>
-      <div className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg m-auto">
+      <div className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg m-auto p-10">
         <img src={CountrySvg} alt="Country SVG" />
       </div>
     </div>
