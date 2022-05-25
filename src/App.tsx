f81f3c194aecdb2bf74fcab40046b032d3af717a
Weike Shi <weikeshih@gmail.com>
2022-05-25 18:39:12 -0400

    Add result modal

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CurrentCountry from "./components/current-country.component";
 import GuessPanels from "./components/guess-panels.component";
 import Selections from "./components/selections.component";
 import Result from "./components/result.component";
-import { IN_PROCESS } from "./constants";
+import { IN_PROCESS, FINISHED_WIN, FINISHED_LOSE } from "./constants";
 
 interface contextType {
   numGuesses: number;
@@ -20,7 +20,7 @@ function App() {
   const [selectedCountries, setSelectedCountries] = useState<
     CountrySelection[]
   >([{}, {}, {}, {}, {}, {}] as CountrySelection[]);
-  const [win, setWin] = useState<String>(IN_PROCESS);
+  const [win, setWin] = useState<String>(FINISHED_LOSE);
 
   const value = {
     numGuesses,
--- src/components/result.component.tsx
+++ src/components/result.component.tsx
@@ -1,14 +1,25 @@
 import { IN_PROCESS, FINISHED_WIN } from "../constants";
 const Result = ({ win }: any) => {
   return (
-    <div>
+    <>
       {win === IN_PROCESS ? null : win === FINISHED_WIN ? (
-        <div>you won</div>
+        <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
+          <div className="flex items-center justify-center z-10 bg-slate-200 p-20 rounded backdrop-blur-md bg-opacity-20">
+            <div>you won</div>
+          </div>
+        </div>
       ) : (
-        <div>you lost</div>
+        <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
+          <div className="flex items-center justify-center z-10 bg-slate-200 p-20 rounded backdrop-blur-md bg-opacity-20">
+            <div>you lost</div>
+          </div>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
 export default Result;
+{
+  /* <div className="absolute w-full h-full">//top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 */
+}
