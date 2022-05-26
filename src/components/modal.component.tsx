61c14dcfa57c293b15d2d51eccb8ab4cc7a892e2
Weike Shi <weikeshih@gmail.com>
2022-05-25 23:04:33 -0400

    Add result modal

---
+++ src/components/modal.component.tsx
@@ -0,0 +1,11 @@
+const Modal = ({ message }: string) => {
+  return (
+    <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
+      <div className="flex items-center justify-center z-10 bg-slate-200 p-20 rounded backdrop-blur-md bg-opacity-20">
+        {message}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/components/result.component.tsx
+++ src/components/result.component.tsx
@@ -1,19 +1,12 @@
 import { IN_PROCESS, FINISHED_WIN } from "../constants";
+import Modal from "./modal.component";
 const Result = ({ win }: any) => {
   return (
     <>
       {win === IN_PROCESS ? null : win === FINISHED_WIN ? (
-        <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
-          <div className="flex items-center justify-center z-10 bg-slate-200 p-20 rounded backdrop-blur-md bg-opacity-20">
-            <div>you won</div>
-          </div>
-        </div>
+        <Modal message={"you win"} />
       ) : (
-        <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
-          <div className="flex items-center justify-center z-10 bg-slate-200 p-20 rounded backdrop-blur-md bg-opacity-20">
-            <div>you lost</div>
-          </div>
-        </div>
+        <Modal message={"you lost"} />
       )}
     </>
   );
