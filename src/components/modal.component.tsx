8e201a0393eb7956e8acc9b440acc0b2c9c6c217
Weike Shi <weikeshih@gmail.com>
2022-05-27 23:37:35 -0400

    Added close button for modal

--- src/components/modal.component.tsx
+++ src/components/modal.component.tsx
@@ -1,11 +1,25 @@
-const Modal = ({ message }: { message: string }) => {
+const Modal = ({
+  message,
+  setOpen,
+}: {
+  message: string;
+  setOpen: (arg0: boolean) => void;
+}) => {
+  // const handleClick = ({ setOpen }: any) => {};
   return (
-    <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
-      <div className="flex items-center justify-center z-10 bg-slate-200 p-20 rounded backdrop-blur-md bg-opacity-20">
-        <div className="absolute top-3 right-3">X</div>
-        <div className="">{message}</div>
+    <>
+      <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
+        <div className="flex items-center justify-center z-10 bg-slate-200 p-20 rounded backdrop-blur-md bg-opacity-20">
+          <button
+            className="absolute top-3 right-3"
+            onClick={() => setOpen(false)} // ADD duncition!!!!!!!!!!!!
+          >
+            X
+          </button>
+          <div className="">{message}</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/result.component.tsx
+++ src/components/result.component.tsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import { IN_PROCESS, FINISHED_WIN } from "../constants";
 import Modal from "./modal.component";
 const Result = ({ win }: any) => {
+  const [open, setOpen] = useState<boolean>(false);
   return (
     <>
       {win === IN_PROCESS ? null : win === FINISHED_WIN ? (
-        <Modal message={"you win"} />
+        <Modal message={"you win"} setOpen={setOpen} />
       ) : (
-        <Modal message={"you lost"} />
+        <Modal message={"you lost"} setOpen={setOpen} />
       )}
     </>
   );
