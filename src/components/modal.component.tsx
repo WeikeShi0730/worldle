d515cd0af3bab1219c8889bee4c8e85bc6775d0e
Weike Shi <weikeshih@gmail.com>
2022-05-30 22:49:38 -0400

    Added close button function for modal

--- src/components/modal.component.tsx
+++ src/components/modal.component.tsx
@@ -5,14 +5,13 @@ const Modal = ({
   message: string;
   setOpen: (arg0: boolean) => void;
 }) => {
-  // const handleClick = ({ setOpen }: any) => {};
   return (
     <>
       <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
         <div className="flex items-center justify-center z-10 bg-slate-200 p-20 rounded backdrop-blur-md bg-opacity-20">
           <button
             className="absolute top-3 right-3"
-            onClick={() => setOpen(false)} // ADD duncition!!!!!!!!!!!!
+            onClick={() => setOpen(false)}
           >
             X
           </button>
--- src/components/result.component.tsx
+++ src/components/result.component.tsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import { IN_PROCESS, FINISHED_WIN } from "../constants";
 import Modal from "./modal.component";
 const Result = ({ win }: any) => {
-  const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(true);
   return (
     <>
-      {win === IN_PROCESS ? null : win === FINISHED_WIN ? (
-        <Modal message={"you win"} setOpen={setOpen} />
-      ) : (
-        <Modal message={"you lost"} setOpen={setOpen} />
-      )}
+      {open ? (
+        win === IN_PROCESS ? null : win === FINISHED_WIN ? (
+          <Modal message={"you win"} setOpen={setOpen} />
+        ) : (
+          <Modal message={"you lost"} setOpen={setOpen} />
+        )
+      ) : null}
     </>
   );
 };
