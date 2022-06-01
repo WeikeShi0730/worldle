ba9dcd8d9b207b3bb83f4bfdbc88a930ee2385eb
Weike Shi <weikeshih@gmail.com>
2022-05-31 23:59:51 -0400

    Add current country wining notification

--- src/components/modal.component.tsx
+++ src/components/modal.component.tsx
@@ -27,7 +27,10 @@ const Modal = ({
     <>
       <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
         <div className="h-full flex flex-col items-center justify-center z-10 bg-slate-200 rounded backdrop-blur-md bg-opacity-20">
-          <button className="absolute top-3 right-3" onClick={handleClickClose}>
+          <button
+            className="self-end p-5"
+            onClick={handleClickClose}
+          >
             X
           </button>
           <div className="p-5">{message}</div>
