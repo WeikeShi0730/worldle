10a9134a06f1cf0227af5149ffce4af0035440a2
Weike Shi <weikeshih@gmail.com>
2022-06-02 23:09:08 -0400

    Clean up div

--- src/components/current-country.component.tsx
+++ src/components/current-country.component.tsx
@@ -2,10 +2,8 @@ import { default as CountrySvg } from "../data/countries-svg/ca/vector.svg";
 
 const CurrentCountry = () => {
   return (
-    <div>
-      <div className="max-w-xs md:max-w-sm lg:max-w-md m-auto p-10">
-        <img src={CountrySvg} alt="Country SVG" />
-      </div>
+    <div className="max-w-xs md:max-w-sm lg:max-w-md m-auto p-10">
+      <img src={CountrySvg} alt="Country SVG" />
     </div>
   );
 };
