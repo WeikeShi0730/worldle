8eb971291c60fcb8b88140ee0228f53396cf3d51
Weike Shi <weikeshih@gmail.com>
2022-06-04 23:32:27 -0400

    Add header component css

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className="flex justify-center items-center text-2xl pt-10 pb-5 mx-20 border-b-2 border-black">
-      WORLDLE
+      WOR"L"DLE
     </div>
   );
 };
