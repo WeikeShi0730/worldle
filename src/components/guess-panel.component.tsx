351982e2ab280e91e254a5da8e98450f6e745dc0
Weike Shi <weikeshih@gmail.com>
2022-06-05 23:40:47 -0400

    Add useGame hook for difference

--- src/components/guess-panel.component.tsx
+++ src/components/guess-panel.component.tsx
@@ -1,11 +1,18 @@
+import useGame from "../utils/useGame";
+
 const GuessPanel = ({ selectedCountry }: any) => {
+  const difference = useGame();
   return (
     <div className="flex h-10 my-1 rounded text-white">
       <div className="country flex justify-center items-center w-8/12 border-2 rounded">
         {selectedCountry.label}
       </div>
-      <div className="distance flex justify-center items-center w-2/12 border-2 mx-1 rounded"></div>
-      <div className="direction flex justify-center items-center w-2/12 border-2 rounded"></div>
+      <div className="distance flex justify-center items-center w-2/12 border-2 mx-1 rounded">
+        {difference.distance}
+      </div>
+      <div className="direction flex justify-center items-center w-2/12 border-2 rounded">
+        {difference.direction}
+      </div>
     </div>
   );
 };
