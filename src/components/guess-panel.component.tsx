0dc7b62c49275d7b3c46325f4962982c196d17b4
Weike Shi <weikeshih@gmail.com>
2022-06-11 23:33:37 -0400

    Fix distance and direction for correct guess

--- src/components/guess-panel.component.tsx
+++ src/components/guess-panel.component.tsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import { CountryType, differenceType } from "../interfaces";
+import { AppContext } from "../App";
 
 type GuessPanelType = {
   selectedCountry: CountryType;
@@ -6,16 +8,24 @@ type GuessPanelType = {
 };
 
 const GuessPanel = ({ selectedCountry, difference }: GuessPanelType) => {
+  const { currentCountry } = useContext(AppContext);
+
   return (
     <div className="flex w-full h-10 my-1 rounded text-white">
       <div className="country flex justify-center items-center w-7/12 border-2 rounded">
         {selectedCountry.label}
       </div>
       <div className="distance flex justify-center items-center w-3/12 border-2 mx-1 rounded">
-        {difference.distance ? difference.distance + "KM" : ""}
+        {selectedCountry.value === currentCountry.value
+          ? "✅"
+          : difference.distance
+          ? difference.distance + "KM"
+          : null}
       </div>
       <div className="direction flex justify-center items-center w-2/12 border-2 rounded">
-        {difference.direction}
+        {selectedCountry.value === currentCountry.value
+          ? "✅"
+          : difference.direction}
       </div>
     </div>
   );
