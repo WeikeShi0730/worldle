34f73582e07cdeaf0aafd12612780e78367cfba8
Weike Shi <weikeshih@gmail.com>
2022-06-11 23:54:54 -0400

    Fix modal css

--- src/components/modal.component.tsx
+++ src/components/modal.component.tsx
@@ -27,7 +27,7 @@ const Modal = ({
   return (
     <>
       <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
-        <div className="h-full flex flex-col items-center justify-center z-10 bg-slate-200 rounded backdrop-blur-md bg-opacity-20">
+        <div className="h-full flex flex-col items-center justify-center z-10 bg-slate-200 text-white rounded backdrop-blur-md bg-opacity-20">
           <button className="self-end p-5" onClick={handleClickClose}>
             X
           </button>
