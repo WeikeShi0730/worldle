a03ac086899efd62ae5ad33d66e4533cb2ca4569
Weike Shi <weikeshih@gmail.com>
2022-06-26 22:06:39 -0400

    Add dynamic import svgs

--- src/components/current-country.component.tsx
+++ src/components/current-country.component.tsx
@@ -1,9 +1,23 @@
-import { default as CountrySvg } from "../data/countries-svg/ca/vector.svg";
+import { useState, useEffect } from "react";
 
 const CurrentCountry = () => {
+  const [countrySvg, setCountrySvg] = useState();
+  useEffect(() => {
+    const dynamicImport = async () => {
+      if (true) {
+        const result = await import("../data/countries-svg/ca/vector.svg");
+        setCountrySvg(result.default as any);
+      }
+    };
+
+    dynamicImport();
+
+    return () => {};
+  }, []);
+
   return (
     <div className="max-w-xs md:max-w-sm lg:max-w-md m-auto p-10">
-      <img src={CountrySvg} alt="Country SVG" />
+      <img src={countrySvg} alt="Country SVG" />
     </div>
   );
 };
