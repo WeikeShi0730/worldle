bcf6e8f255ed78a19744e59a17504d0ab9a694a5
Weike Shi <weikeshih@gmail.com>
2022-06-28 23:55:55 -0400

    Add css to result modal

--- src/components/modal.component.tsx
+++ src/components/modal.component.tsx
@@ -20,26 +20,29 @@ const Modal = ({ setOpen }: { setOpen: (open: boolean) => void }) => {
   };
 
   return (
-    <>
-      <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
-        <div className="h-full flex flex-col items-center justify-center z-10 bg-slate-200 text-white rounded backdrop-blur-md bg-opacity-20">
-          <button className="self-end p-5" onClick={handleClickClose}>
-            X
-          </button>
+    <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
+      <div className="h-full flex flex-col items-center justify-center m-10 bg-slate-200 text-white rounded-lg backdrop-blur-md bg-opacity-20 shadow-md">
+        <button
+          className="self-end m-5 text-2xl hover:bg-opacity-50 duration-200 hover:text-slate-700"
+          onClick={handleClickClose}
+        >
+          X
+        </button>
+        <div className="px-16 flex flex-col items-center justify-center">
           {game === FINISHED_WIN ? (
             <div className="p-5">win</div>
           ) : (
             <div className="p-5">lose</div>
           )}
           <button
-            className="self-center mt-auto p-5"
+            className="m-5 p-5 bg-slate-700 bg-opacity-70 shadow rounded-lg hover:bg-opacity-50 hover:shadow-md duration-200"
             onClick={handleClickNewGame}
           >
             New Game
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
