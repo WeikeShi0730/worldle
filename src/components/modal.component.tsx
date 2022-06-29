7d7016d4472ce1685ae559947f52694c76591281
Weike Shi <weikeshih@gmail.com>
2022-06-29 00:13:36 -0400

    Fix modal message

--- src/components/modal.component.tsx
+++ src/components/modal.component.tsx
@@ -4,8 +4,15 @@ import { FINISHED_WIN, IN_PROCESS } from "../constants";
 import { CountryType, differenceType } from "../interfaces";
 
 const Modal = ({ setOpen }: { setOpen: (open: boolean) => void }) => {
-  const { game, setGame, setNumGuesses, setSelectedCountries, setDifference } =
-    useContext(AppContext);
+  const {
+    currentCountry,
+    game,
+    setGame,
+    numGuesses,
+    setNumGuesses,
+    setSelectedCountries,
+    setDifference,
+  } = useContext(AppContext);
 
   const handleClickNewGame = () => {
     setOpen(false);
@@ -19,6 +26,28 @@ const Modal = ({ setOpen }: { setOpen: (open: boolean) => void }) => {
     setOpen(false);
   };
 
+  const message = () => {
+    return game === FINISHED_WIN ? (
+      numGuesses === 1 ? (
+        <div className="p-5">
+          You got it on the <span className="text-xl italic">first</span> try!
+        </div>
+      ) : numGuesses === 6 ? (
+        <div className="p-5">Oof, that was close.</div>
+      ) : (
+        <div className="p-5">
+          You got it in <span className="text-xl italic">{numGuesses} </span>
+          guesses.
+        </div>
+      )
+    ) : (
+      <div className="p-5 text-center">
+        You didn't get it, the country is
+        <div className="text-xl italic">{currentCountry.label}.</div>
+      </div>
+    );
+  };
+
   return (
     <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
       <div className="h-full flex flex-col items-center justify-center m-10 bg-slate-200 text-white rounded-lg backdrop-blur-md bg-opacity-20 shadow-md">
@@ -29,11 +58,7 @@ const Modal = ({ setOpen }: { setOpen: (open: boolean) => void }) => {
           X
         </button>
         <div className="px-16 flex flex-col items-center justify-center">
-          {game === FINISHED_WIN ? (
-            <div className="p-5">win</div>
-          ) : (
-            <div className="p-5">lose</div>
-          )}
+          {message()}
           <button
             className="m-5 p-2 w-40 bg-slate-700 bg-opacity-70 shadow rounded hover:bg-opacity-50 hover:shadow-md duration-200"
             onClick={handleClickNewGame}
