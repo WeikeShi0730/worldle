641450937ad2076055834684f481170882ef447a
Weike Shi <weikeshih@gmail.com>
2022-06-29 23:25:47 -0400

    Fix footer css

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -2,9 +2,9 @@ import { SiLinkedin, SiGithub, SiTwitter } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col m-10">
+    <div className="flex flex-col m-10 pt-5 pb-10 text-white">
       <div className="text-xs md:text-sm text-center">create by</div>
-      <div className="text-xs md:text-sm flex justify-center gap-x-1">
+      <div className="text-xs md:text-sm flex justify-center items-center gap-x-1">
         <a
           href="https://www.linkedin.com/in/weike-shi/"
           rel="noopener noreferrer"
@@ -14,7 +14,7 @@ const Footer = () => {
         </a>
         <SiLinkedin />
       </div>
-      <div className="text-xs md:text-sm flex justify-center gap-x-1">
+      <div className="text-xs md:text-sm flex justify-center items-center gap-x-1">
         <a
           href="https://github.com/WeikeShi0730/worldle"
           rel="noopener noreferrer"
@@ -24,7 +24,7 @@ const Footer = () => {
         </a>
         <SiGithub />
       </div>
-      <div className="text-xs md:text-sm text-center flex justify-center gap-x-1">
+      <div className="text-xs md:text-sm text-center flex justify-center items-center gap-x-1">
         <a
           href="https://twitter.com/vicshi97"
           rel="noopener noreferrer"
