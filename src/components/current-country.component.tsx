2a0d0b44a7ec2480fca23530f8493ce3472484fa
Weike Shi <weikeshih@gmail.com>
2022-07-06 23:02:57 -0400

    Fix svg display position

--- src/components/current-country.component.tsx
+++ src/components/current-country.component.tsx
@@ -5,7 +5,7 @@ const CurrentCountry = () => {
   return (
     <div className="w-full h-full max-w-xs m-auto p-5">
       {countrySvg ? (
-        <div className="flex justify-center items-center gap-x-5">
+        <div className="h-full flex justify-center items-center gap-x-5">
           <img src={countrySvg.shape} alt="Country SVG" />
           <img src={countrySvg.flag} alt="Country SVG" />
         </div>
