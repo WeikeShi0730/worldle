1e8e5310f7109f7702f8b3a7ca5e5ab56a494fe1
Weike Shi <weikeshih@gmail.com>
2022-07-08 23:02:39 -0400

    Add card flip

--- src/components/current-country.component.tsx
+++ src/components/current-country.component.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import useNewCountry from "../utils/useNewCountry";
+import ReactCardFlip from "react-card-flip";
 
 const CurrentCountry = () => {
   const countrySvg = useNewCountry();
+  console.log(countrySvg);
+  const [flipped, setFlipped] = useState<boolean>(false);
+
+  const handleClick = (event: any) => {
+    event.preventDefault();
+    setFlipped((flipped) => !flipped);
+  };
   return (
     <div className="w-full h-full max-w-xs m-auto p-5">
       {countrySvg ? (
         <div className="h-full flex justify-center items-center gap-x-5">
-          <img src={countrySvg.shape} alt="Country SVG" />
-          <img src={countrySvg.flag} alt="Country SVG" />
+          <ReactCardFlip isFlipped={flipped}>
+            <div className="">
+              <img src={countrySvg.shape} alt="Country Shape SVG" />
+              <button onClick={handleClick}>click</button>
+            </div>
+            <div className="">
+              <img src={countrySvg.flag} alt="Country Flag SVG" />
+              <button onClick={handleClick}>click</button>
+            </div>
+          </ReactCardFlip>
         </div>
       ) : null}
     </div>
