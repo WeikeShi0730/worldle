3ed5dd25d8e8d3191d0e8a8f6299f85066662979
Weike Shi <weikeshih@gmail.com>
2022-07-20 00:15:51 -0400

    Add modal to instruction click

--- src/components/instruction.component.tsx
+++ src/components/instruction.component.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 const Instruction = () => {
-  return <div>?</div>;
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setOpen(() => !open);
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={handleClick}>?</button>
+      </div>
+      {open ? (
+        <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 w-20 h-20 bg-slate-600">
+
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default Instruction;
