edfb4c5be8a0fdbd31d9ffbdad042de97f4d5869
Weike Shi <weikeshih@gmail.com>
2022-07-21 00:03:33 -0400

    Fix header title and ? position css

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -3,7 +3,7 @@ import Instruction from "./instruction.component";
 const Header = () => {
   return (
     <div className="flex justify-between items-center text-2xl pt-10 pb-5 mx-20 text-white border-b-2 border-white">
-      <div />
+      <div className="opacity-0">?</div>
       <div className="">WORLDLE</div>
       <div className="">
         <Instruction />
--- src/components/instruction.component.tsx
+++ src/components/instruction.component.tsx
@@ -7,6 +7,7 @@ const Instruction = () => {
     event.preventDefault();
     setOpen(() => !open);
   };
+//   const handleClickClose = (event: React.MouseEvent<HTMLElement>) => {};
   return (
     <div>
       <div>
@@ -14,7 +15,13 @@ const Instruction = () => {
       </div>
       {open ? (
         <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-slate-200 text-white rounded-lg backdrop-blur-md bg-opacity-20 shadow-md">
-          <div className="m-10">
+          <div className="flex m-10">
+            {/* <button
+              className="self-end m-5 text-2xl hover:bg-opacity-50 duration-200 hover:text-slate-700"
+              onClick={handleClickClose}
+            >
+              X
+            </button> */}
             <p>How to play</p>
           </div>
         </div>
