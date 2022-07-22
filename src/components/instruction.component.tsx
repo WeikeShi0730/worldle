b970d56fef03b739fff754a53349a5362021fbb4
Weike Shi <weikeshih@gmail.com>
2022-07-21 22:45:45 -0400

    Add X to instruction modal

--- src/components/instruction.component.tsx
+++ src/components/instruction.component.tsx
@@ -7,7 +7,10 @@ const Instruction = () => {
     event.preventDefault();
     setOpen(() => !open);
   };
-//   const handleClickClose = (event: React.MouseEvent<HTMLElement>) => {};
+  const handleClickClose = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setOpen(false);
+  };
   return (
     <div>
       <div>
@@ -15,13 +18,13 @@ const Instruction = () => {
       </div>
       {open ? (
         <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-slate-200 text-white rounded-lg backdrop-blur-md bg-opacity-20 shadow-md">
-          <div className="flex m-10">
-            {/* <button
+          <div className="flex flex-col m-10">
+            <button
               className="self-end m-5 text-2xl hover:bg-opacity-50 duration-200 hover:text-slate-700"
               onClick={handleClickClose}
             >
               X
-            </button> */}
+            </button>
             <p>How to play</p>
           </div>
         </div>
