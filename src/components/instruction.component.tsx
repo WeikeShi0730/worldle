a316c5caa4e928a12afd29f609280c10cdad629b
Weike Shi <weikeshih@gmail.com>
2022-07-24 21:50:53 -0400

    Added content to instruction

--- src/components/instruction.component.tsx
+++ src/components/instruction.component.tsx
@@ -25,7 +25,20 @@ const Instruction = () => {
             >
               X
             </button>
-            <p>How to play</p>
+            <p className="">How to play</p>
+            <br />
+            <p>
+              Guess the WORLDLE in 6 guesses. Each guess must be a valid
+              country, territory, ... After each guess, you will be given a
+              distance and direction hint to the target country.
+            </p>
+            <p>
+              You can enable the option to click on the map to show the flag of
+              the country, which would lower the difficulty of the game.
+            </p>
+            <p>Example:</p>
+            <div className=""></div>
+
           </div>
         </div>
       ) : null}
