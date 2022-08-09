21879269f01c92a8870932d94ca9c32eaa780cdd
Weike Shi <weikeshih@gmail.com>
2022-08-08 23:09:09 -0400

    Add modal for settings

--- src/components/setttings.component.tsx
+++ src/components/setttings.component.tsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+
 const Settings = () => {
-  return <div>⚙</div>;
+  const [open, setOpen] = useState<boolean>(false);
+  const handleMarkClick = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setOpen(() => !open);
+  };
+  const handleClickClose = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setOpen(false);
+  };
+  return (
+    <div>
+      <div>
+        <div className="hover:bg-opacity-50 duration-200 text-slate-300 hover:text-white font-light">
+          <button onClick={handleMarkClick}>⚙</button>
+        </div>
+        {open ? (
+          <div className="fixed w-96 z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-slate-200 text-white text-base rounded-lg backdrop-blur-md bg-opacity-20 shadow-md">
+            <div className="flex flex-col m-5 sm:m-10">
+              <button
+                className="self-end text-2xl hover:bg-opacity-50 duration-200 hover:text-slate-700 font-light"
+                onClick={handleClickClose}
+              >
+                X
+              </button>
+              <p className="border-b text-center text-2xl">Settings</p>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
