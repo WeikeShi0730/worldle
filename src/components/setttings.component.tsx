07386db515f7166bf24d654af0bf17ba791be7d0
Weike Shi <weikeshih@gmail.com>
2022-08-10 23:43:07 -0400

    Add setting toggle button

--- src/components/setttings.component.tsx
+++ src/components/setttings.component.tsx
@@ -16,8 +16,7 @@ const Settings = () => {
     event.preventDefault();
     setOpen(false);
   };
-  const handleToggle = (event: React.MouseEvent<HTMLElement>) => {
-    event.preventDefault();
+  const handleToggle = () => {
     setEnableFlag(!enableFlag);
   };
   return (
@@ -38,12 +37,15 @@ const Settings = () => {
             <br />
             <div className="flex items-center justify-between">
               <div className="">Flip to see flags</div>
-              <button
-                className="font-light p-2 bg-orange-500"
-                onClick={handleToggle}
-              >
-                Toggle
-              </button>
+              <label className="inline-flex relative items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  className="sr-only peer"
+                  onChange={handleToggle}
+                  checked={enableFlag}
+                />
+                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              </label>
             </div>
           </div>
         </div>
