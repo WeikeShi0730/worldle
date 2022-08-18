a8b0d4043470e8be31cbabe7849a6f6f1cfa09d6
Weike Shi <weikeshih@gmail.com>
2022-08-17 22:03:40 -0400

    Fix modal css

--- src/components/modal.component.tsx
+++ src/components/modal.component.tsx
@@ -29,38 +29,42 @@ const Modal = ({ setOpen }: { setOpen: (open: boolean) => void }) => {
   const message = () => {
     return game === FINISHED_WIN ? (
       numGuesses === 1 ? (
-        <div className="p-5">
-          You got it on the <span className="text-xl italic">first</span> try!
+        <div className="p-5 text-sm md:text-base">
+          You got it on the{" "}
+          <span className="text-base md:text-xl italic">first</span> try!
         </div>
       ) : numGuesses === 6 ? (
-        <div className="p-5">Oof, that was close.</div>
+        <div className="p-5 text-sm md:text-base">Oof, that was close.</div>
       ) : (
-        <div className="p-5">
-          You got it in <span className="text-xl italic">{numGuesses} </span>
+        <div className="p-5 text-sm md:text-base">
+          You got it in{" "}
+          <span className="text-base md:text-xl italic">{numGuesses} </span>
           guesses.
         </div>
       )
     ) : (
-      <div className="p-5 text-center">
+      <div className="p-5 text-sm md:text-base text-center">
         You didn't get it, the country is
-        <div className="text-xl italic">{currentCountry.label}.</div>
+        <div className="text-base md:text-xl italic">
+          {currentCountry.label}.
+        </div>
       </div>
     );
   };
 
   return (
-    <div className="fixed z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
-      <div className="h-full flex flex-col items-center justify-center m-10 bg-slate-200 text-white rounded-lg backdrop-blur-md bg-opacity-20 shadow-md">
+    <div className="fixed w-80 md:w-96 z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-slate-200 text-white text-base rounded-lg backdrop-blur-md bg-opacity-20 shadow-md">
+      <div className="flex flex-col m-5 sm:m-10 text-white">
         <button
-          className="self-end m-5 text-2xl hover:bg-opacity-50 duration-200 hover:text-slate-700 font-light"
+          className="self-end m-5 hover:bg-opacity-50 duration-200 hover:text-slate-700 font-light text-lg md:text-2xl"
           onClick={handleClickClose}
         >
           X
         </button>
-        <div className="px-16 flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
           {message()}
           <button
-            className="m-5 p-2 w-40 bg-slate-700 bg-opacity-70 shadow rounded hover:bg-opacity-50 hover:shadow-md duration-200 font-light"
+            className="m-5 p-2 w-40 bg-slate-700 bg-opacity-70 shadow rounded hover:bg-opacity-50 hover:shadow-md duration-200 font-light text-sm md:text-base"
             onClick={handleClickNewGame}
           >
             New Game
