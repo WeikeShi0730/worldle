a31a7c097ad8c669489911a710f457bd26672321
Weike Shi <weikeshih@gmail.com>
2022-08-22 23:05:24 -0400

    Add click outside for result modal

--- src/components/modal.component.tsx
+++ src/components/modal.component.tsx
@@ -1,7 +1,8 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { AppContext } from "../App";
 import { FINISHED_WIN, IN_PROCESS } from "../constants";
 import { CountryType, differenceType } from "../interfaces";
+import { useClickOutside } from "../utils/useClickOutside";
 
 const Modal = ({ setOpen }: { setOpen: (open: boolean) => void }) => {
   const {
@@ -13,6 +14,8 @@ const Modal = ({ setOpen }: { setOpen: (open: boolean) => void }) => {
     setSelectedCountries,
     setDifference,
   } = useContext(AppContext);
+  const ref = useRef<HTMLDivElement>(null);
+  useClickOutside(ref, () => setOpen(false));
 
   const handleClickNewGame = () => {
     setOpen(false);
@@ -53,7 +56,10 @@ const Modal = ({ setOpen }: { setOpen: (open: boolean) => void }) => {
   };
 
   return (
-    <div className="fixed w-80 md:w-96 z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-slate-200 text-white text-base rounded-lg backdrop-blur-md bg-opacity-20 shadow-md">
+    <div
+      ref={ref}
+      className="fixed w-80 md:w-96 z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-slate-200 text-white text-base rounded-lg backdrop-blur-md bg-opacity-20 shadow-md"
+    >
       <div className="flex flex-col m-5 sm:m-10 text-white">
         <button
           className="self-end m-5 hover:bg-opacity-50 duration-200 hover:text-slate-700 font-light text-lg md:text-2xl"
