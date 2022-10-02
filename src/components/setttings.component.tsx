184506b012d51c1b765f9c15a6b41a9a94c1e7c0
Weike Shi <weikeshih@gmail.com>
2022-10-02 00:25:19 -0400

    fix setting toggle css

--- src/components/setttings.component.tsx
+++ src/components/setttings.component.tsx
@@ -69,16 +69,19 @@ const Settings = () => {
             <br />
             <div className="flex items-center justify-between text-sm md:text-base">
               <div className="">Flip to see flags</div>
-              <label className="inline-flex relative items-center cursor-pointer">
-                <input
-                  type="checkbox"
-                  className="sr-only peer"
-                  onChange={handleToggle}
-                  checked={enableFlag}
-                  value="flag"
-                />
-                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-              </label>
+              <div className="flex gap-x-4">
+                <label className="inline-flex relative items-center cursor-pointer">
+                  <input
+                    type="checkbox"
+                    className="sr-only peer"
+                    onChange={handleToggle}
+                    checked={enableFlag}
+                    value="flag"
+                  />
+                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                </label>
+                <span className="invisible">km</span>
+              </div>
             </div>
           </div>
         </div>
