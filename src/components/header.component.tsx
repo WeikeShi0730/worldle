3f42c78b2265e05bb388a5279cf750ffe3da901f
Weike Shi <weikeshih@gmail.com>
2022-10-02 22:38:49 -0400

    update header width

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -3,7 +3,7 @@ import Settings from "./setttings.component";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center text-2xl pt-10 pb-5 mx-20 text-white border-b-2 border-white">
+    <div className="flex justify-between items-center text-2xl pt-10 pb-5 mx-10 text-white border-b-2 border-white">
       <Instruction />
       <div className="">WORLDLE</div>
       <Settings />
