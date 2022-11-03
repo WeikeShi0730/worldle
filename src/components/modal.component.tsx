85667023795f341420b985591e13719942f6b0d1
Weike Shi <weikeshih@gmail.com>
2022-11-02 23:56:27 -0400

    Change Modal transition css

--- src/components/modal.component.tsx
+++ src/components/modal.component.tsx
@@ -9,11 +9,21 @@ const Modal = ({
 }) => {
   const handleClickClose = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
+    var element = document.getElementById("modal");
+    element?.classList.remove("invisible");
+    element?.classList.remove("opacity-0");
+    setTimeout(() => {
+      element?.classList.add("invisible");
+      element?.classList.add("opacity-0");
+    }, 1500);
     setOpen(false);
   };
 
   return (
-    <div className="fixed w-80 md:w-96 z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-slate-200 text-white text-base rounded-lg backdrop-blur-md bg-opacity-20 shadow-md">
+    <div
+      id="modal"
+      className="opacity-0 invisible transform duration-200 ease-in-out fixed w-80 md:w-96 z-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-slate-200 text-white text-base rounded-lg backdrop-blur-md bg-opacity-20 shadow-md"
+    >
       <div className="flex flex-col m-5 sm:m-10 text-white text-sm md:text-base">
         <button
           className="flex content-center items-center self-end text-lg md:text-2xl hover:bg-opacity-50 duration-200 hover:text-slate-700 font-light"
