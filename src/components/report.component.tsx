5c905038ef63a05b41aed16e10f0326d6325164f
Weike Shi <weikeshih@gmail.com>
2022-11-20 21:25:53 -0500

    Added statistics function

--- src/components/report.component.tsx
+++ src/components/report.component.tsx
@@ -1,17 +1,74 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect, useContext } from "react";
+import { AppContext } from "../App";
 import { useClickOutside } from "../utils/useClickOutside";
 import Modal from "./modal.component";
 import { VscGraphLine } from "react-icons/vsc";
+import { useCookies } from "react-cookie";
+import { FINISHED_WIN, FINISHED_LOSE, IN_PROCESS } from "../constants";
 
 const Report = () => {
   const [open, setOpen] = useState<boolean>(false);
+  const [statistics, setStatistics] = useState<{
+    currentStreak: number;
+    maxStreak: number;
+    totalPlayed: number;
+    totalWin: number;
+    winPerc: number;
+    history: number[];
+  }>({
+    currentStreak: 0,
+    maxStreak: 0,
+    totalPlayed: 0,
+    totalWin: 0,
+    winPerc: 0,
+    history: [],
+  });
   const ref = useRef<HTMLDivElement>(null);
   useClickOutside(ref, () => setOpen(false));
+  const { game, random } = useContext(AppContext);
 
   const handleMarkClick = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
     setOpen(() => !open);
   };
+
+  const [cookies, setCookie] = useCookies();
+
+  useEffect(() => {
+    const cockieFound = cookies && cookies.statistics;
+    var currentStreak = cockieFound ? cookies.statistics.currentStreak : 0;
+    var maxStreak = cockieFound ? cookies.statistics.maxStreak : 0;
+    var totalPlayed = cockieFound ? cookies.statistics.totalPlayed : 0;
+    var totalWin = cockieFound ? cookies.statistics.totalWin : 0;
+    var history = cockieFound ? cookies.statistics.history : [];
+    if (game !== IN_PROCESS && !history.includes(random)) {
+      totalPlayed += 1;
+      if (game === FINISHED_LOSE) {
+        currentStreak = 0;
+      } else if (game === FINISHED_WIN) {
+        currentStreak += 1;
+        totalWin += 1;
+        if (currentStreak > maxStreak) {
+          maxStreak = currentStreak;
+        }
+      }
+      var winPerc = totalWin / totalPlayed;
+      history.push(random);
+      const statistics = {
+        currentStreak,
+        maxStreak,
+        totalPlayed,
+        totalWin,
+        winPerc,
+        history,
+      };
+      setStatistics(statistics);
+
+      setCookie("statistics", JSON.stringify(statistics), {
+        maxAge: 86400,
+      });
+    }
+  }, [game]);
 
   return (
     <div ref={ref}>
@@ -22,7 +79,7 @@ const Report = () => {
       </div>
       <Modal setOpen={setOpen} open={open} id="modal-report">
         <p className="w-full border-b text-center text-lg md:text-2xl">
-          How to play
+          Statistics
         </p>
         <br />
         <p>
